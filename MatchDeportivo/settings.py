8977a6767fa41b1962f97fcfd66fb09347048bf6
EdderZurita <kyo_19@live.com>
2025-12-10 20:24:16 -0300

    fix: Comentar context processor de Google Maps que causa error 500

--- MatchDeportivo/settings.py
+++ MatchDeportivo/settings.py
@@ -80,7 +80,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'MatchDeportivoAPP.context_processors.google_maps_key',
+                # 'MatchDeportivoAPP.context_processors.google_maps_key',  # Comentado temporalmente
             ],
         },
     },
